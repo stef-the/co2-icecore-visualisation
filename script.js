d6dfd991df9b8e6afeb5c2bdbb8bad6522190ad9
stef <stefthedoggo4@gmail.com>
2022-12-10 18:28:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let parsed = [];
+let chartdata = [];
+const that = "<b><a href=\"https://gml.noaa.gov/aftp/data/trace_gases/co2/flask/surface/txt/co2_mlo_surface-flask_1_ccgg_month.txt\">this</a></b>"
 $.ajax({
   url: "data.txt",
   success: function (data) {
     const lists = data.split("\n");
     let fragment = [];
-    let parsed = [];
     lists.forEach((element) => {
       if (element[0] !== "#") {
         fragment.push(element.split("\r")[0].split(" "));
@@ -13,34 +15,34 @@ $.ajax({
       let i = element.length;
       while (i--) {
         if (element[i] === "") {
-            element.splice(i, 1);
+          element.splice(i, 1);
         }
       }
-      parsed.push(element)
+      parsed.push(element.slice(1, 4));
     });
-    console.log(parsed);
-  },
-});
+    parsed.forEach((element) => {
+      chartdata.push([Number(new Date(`${element[0]}-${element[1]}`).getTime()),Number(element[2])]);
+    });
 
-Highcharts.getJSON("data.json", function (data) {
-  // Create the chart
-  Highcharts.stockChart("container", {
-    rangeSelector: {
-      selected: 1,
-    },
-
-    title: {
-      text: "Global Atmoshperic Carbon Dioxide (CO2)",
-    },
-
-    series: [
-      {
-        name: "CO2",
-        data: data,
-        tooltip: {
-          valueDecimals: 2,
+    console.log(chartdata)
+    Highcharts.stockChart("container", {
+        rangeSelector: {
+          selected: 1,
         },
-      },
-    ],
-  });
+      
+        title: {
+          text: "Global Atmoshperic Carbon Dioxide (CO2) according to "+that,
+        },
+      
+        series: [
+          {
+            name: "CO2",
+            data: chartdata,
+            tooltip: {
+              valueDecimals: 2,
+            },
+          },
+        ],
+      });
+  },
 });
